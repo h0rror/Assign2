3a5f680281f2ac70014e516248bac91803cc76a7
h0rror <spiceace@gmail.com>
2016-05-20 11:17:50 -0400

    commit 2, added test

---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void addYtoXTestMethod()
+        {
+            //Arrange
+            int x = 40;
+            int y = 50;
+            int expectedResult = 90;
+
+            //Act
+            int result = Program.addYtoX(ref x, ref y);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
